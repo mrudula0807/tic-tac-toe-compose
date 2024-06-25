6b24b68ac3825921be37f9c4ba8ee4779af0931a
mrudula0807 <mrudula8792@gmail.com>
2024-06-25 09:50:01 +0530

    Out of space in landscape fix

--- app/src/main/java/com/msk/tictactoe/ui/components/TicTacToeGame.kt
+++ app/src/main/java/com/msk/tictactoe/ui/components/TicTacToeGame.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -30,7 +32,8 @@ fun TicTacToeGame(viewModel: TicTacToeViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
